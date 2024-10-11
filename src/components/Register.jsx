77f5de84d20fd9c13fbf4ce021d951e23f2ee78b
godevx <tailorg664@gmail.com>
2024-10-12 00:24:56 +0530

    register.css.modifications

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,9 +3,9 @@ import logo from "../logo/Logo.jpg";
 function Register() {
   return (
     <div className="main-container bg-gray-400">
-      <div className="register-container flex bg-gray-100">
+      <div className="register-container flex rounded-3xl overflow-hidden bg-white">
         <div className="logo flex">
-          <img src={logo} alt="" className="static"/>
+          <img src={logo} alt="" className="static" />
           <h2 className="text-2xl pt-5 z-10">Clover Book</h2>
         </div>
       </div>
