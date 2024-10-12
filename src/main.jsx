def646fdcba9daa8a03d5037bb67dc73e44b6bef
godevx <tailorg664@gmail.com>
2024-10-12 16:59:28 +0530

    router.initialization_gt

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromChildren,
+  Outlet,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { Dashboard, Register, Signin } from "./components/index.js";
+// import App from './App.jsx'
+import "./index.css";
+//router creation
+const router = createBrowserRouter(
+  createRoutesFromChildren(
+    <>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="register" element={<Register />} />
+      <Route path="signin" element={<Signin />} />
+    </>
+  )
+);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
